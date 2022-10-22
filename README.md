adeca31e09f6f49e0e007b487fa3886b95489d32
JuTemp <111566853+JuTemp@users.noreply.github.com>
2022-10-22 14:33:01 +0800

    Commit 2022Oct22

--- README.md
+++ README.md
@@ -1,1 +1,24 @@
-# NjtechHomeConnect
+It is finished at 2022Oct22.
+
+The script is for bash platform.
+You need to put the two files at the same directory.
+
+```
+$ cd $DIR
+$ bash login.bash
+```
+
+If it work correctly, it will output like this
+
+```
+F28D34EA658A1607295773FFC3D62146.TomcatB 67313298 LT-2084105-CAIhK5iXslhnkqzgD3qBrNStfp0BtM e1s1 6296 ea0335137fc3c281aaa639a970678571b4224e7e
+Login Success.
+```
+
+and input another two files "NjtechHomeC1Html" and "captcha.jpg", which you need not delete them.
+
+
+If it cannot work, you can contact me.
+
+----JuTemp 2022Oct22
+
---
+++ login.bash
@@ -0,0 +1,25 @@
+#!/bin/sh
+
+curl -i -s -L 'https://u.njtech.edu.cn/cas/login?service=https%3A%2F%2Fu.njtech.edu.cn%2Foauth2%2Fauthorize%3Fclient_id%3DOe7wtp9CAMW0FVygUasZ%26response_type%3Dcode%26state%3Dnjtech%26s%3Df682b396da8eb53db80bb072f5745232' > NjtechHomeC1Html
+
+para[1]=`sed -n -r '1,20s/^.*?JSESSIONID=(.*?);.*?;.*?$/\1/gp' NjtechHomeC1Html`
+para[2]=`sed -n -r '1,20s/^.*?insert_cookie=([0-9]{8});.*?$/\1/gp' NjtechHomeC1Html`
+para[3]=`sed -n -r '1,200s/^.*?name\=\"lt\" value\=\"(.*?)\"\/>.*?$/\1/gp' NjtechHomeC1Html`
+para[4]=`sed -n -r '1,200s/^.*?name\=\"execution\" value\=\"(.*?)\"\/>.*?$/\1/gp' NjtechHomeC1Html`
+
+curl -s 'https://u.njtech.edu.cn/cas/captcha.jpg?0.29064673903582183' -H "Cookie: JSESSIONID=${para[1]}; insert_cookie=${para[2]}" --compressed  --output captcha.jpg
+shasumCaptcha=`sha1sum captcha.jpg | cut -d ' ' -f 1`
+para[5]=`sed -n "/^$shasumCaptcha/p" RainbowNjtechHomeKeys | cut -d ' ' -f 2`
+
+while [ -z ${para[5]} ]; do
+  curl -s 'https://u.njtech.edu.cn/cas/captcha.jpg?0.29064673903582183' -H "Cookie: JSESSIONID=${para[1]}; insert_cookie=${para[2]}" --compressed  --output captcha.jpg
+  shasumCaptcha=`sha1sum captcha.jpg | cut -d ' ' -f 1`
+  para[5]=`sed -n "/^$shasumCaptcha/p" RainbowNjtechHomeKeys | cut -d ' ' -f 2`
+done
+
+echo ${para[@]} $shasumCaptcha
+
+curl -s -XPOST "https://u.njtech.edu.cn/cas/login;jsessionid=${para[1]}?service=https%3A%2F%2Fu.njtech.edu.cn%2Foauth2%2Fauthorize%3Fclient_id%3DOe7wtp9CAMW0FVygUasZ%26response_type%3Dcode%26state%3Dnjtech%26s%3Df682b396da8eb53db80bb072f5745232" -H "Cookie: JSESSIONID=${para[1]}; insert_cookie=${para[2]}" -d "username=202221149009&password=QIUYE-Njtech&captcha=${para[5]}&channelshow=%E4%B8%AD%E5%9B%BD%E7%94%B5%E4%BF%A1&channel=%40telecom&lt=${para[3]}&execution=${para[4]}&_eventId=submit" \ --compressed  -L | grep -o 'window.location.href = "/oauth2/logout?retUrl=https://i.njtech.edu.cn";' > /dev/null
+
+if [ $? -eq 0 ] ; then echo "Login Success." ; else echo "Login Fail. Retrying." ; bash login.bash ; fi
+
