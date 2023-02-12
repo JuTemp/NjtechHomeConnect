d701cc3d326e0f2374df3a817c95f14a493931e7
JuTemp <111566853+JuTemp@users.noreply.github.com>
2023-02-12 22:38:02 +0800

    Commit230212

--- login.bash
+++ login.bash
@@ -3,26 +3,33 @@
 username="202221140000"
 password="passwd123456"
 
-curl -i -s -L 'https://u.njtech.edu.cn/cas/login?service=https%3A%2F%2Fu.njtech.edu.cn%2Foauth2%2Fauthorize%3Fclient_id%3DOe7wtp9CAMW0FVygUasZ%26response_type%3Dcode%26state%3Dnjtech%26s%3Df682b396da8eb53db80bb072f5745232' > NjtechHomeC1Html
+# 中国电信
+channelshow="%E4%B8%AD%E5%9B%BD%E7%94%B5%E4%BF%A1"
+channel="%40telecom"
 
-para[1]=`sed -n -r '1,20s/^.*?JSESSIONID=(.*?);.*?;.*?$/\1/gp' NjtechHomeC1Html`
-para[2]=`sed -n -r '1,20s/^.*?insert_cookie=([0-9]{8});.*?$/\1/gp' NjtechHomeC1Html`
-para[3]=`sed -n -r '1,200s/^.*?name\=\"lt\" value\=\"(.*?)\"\/>.*?$/\1/gp' NjtechHomeC1Html`
-para[4]=`sed -n -r '1,200s/^.*?name\=\"execution\" value\=\"(.*?)\"\/>.*?$/\1/gp' NjtechHomeC1Html`
+# 中国移动
+channelshow="%E4%B8%AD%E5%9B%BD%E7%A7%BB%E5%8A%A8"
+channel="%40cmcc"
 
-curl -s 'https://u.njtech.edu.cn/cas/captcha.jpg?0.29064673903582183' -H "Cookie: JSESSIONID=${para[1]}; insert_cookie=${para[2]}" --compressed  --output captcha.jpg
+curl -i -s -L 'https://u.njtech.edu.cn/cas/login?service=https%3A%2F%2Fu.njtech.edu.cn%2Foauth2%2Fauthorize%3Fclient_id%3DOe7wtp9CAMW0FVygUasZ%26response_type%3Dcode%26state%3Dnjtech%26s%3Df682b396da8eb53db80bb072f5745232' > /home/jtp/ConnectNjtechHome/NjtechHomeHtml
+
+para[1]=`sed -n -r '1,20s/^.*?JSESSIONID=(.*?);.*?;.*?$/\1/gp' /home/jtp/ConnectNjtechHome/NjtechHomeHtml`
+para[2]=`sed -n -r '1,20s/^.*?insert_cookie=([0-9]{8});.*?$/\1/gp' /home/jtp/ConnectNjtechHome/NjtechHomeHtml`
+para[3]=`sed -n -r '1,200s/^.*?name\=\"lt\" value\=\"(.*?)\"\/>.*?$/\1/gp' /home/jtp/ConnectNjtechHome/NjtechHomeHtml`
+para[4]=`sed -n -r '1,200s/^.*?name\=\"execution\" value\=\"(.*?)\"\/>.*?$/\1/gp' /home/jtp/ConnectNjtechHome/NjtechHomeHtml`
+
+curl -s 'https://u.njtech.edu.cn/cas/captcha.jpg?0.29064673903582183' -H "Cookie: JSESSIONID=${para[1]}; insert_cookie=${para[2]}" --output captcha.jpg
 shasumCaptcha=`sha1sum captcha.jpg | cut -d ' ' -f 1`
-para[5]=`sed -n "/^$shasumCaptcha/p" RainbowNjtechHomeKeys | cut -d ' ' -f 2`
+para[5]=`sed -n "/^$shasumCaptcha/p" /home/jtp/ConnectNjtechHome/RainbowNjtechHomeKeys | cut -d ' ' -f 2`
 
 while [ -z ${para[5]} ]; do
-  curl -s 'https://u.njtech.edu.cn/cas/captcha.jpg?0.29064673903582183' -H "Cookie: JSESSIONID=${para[1]}; insert_cookie=${para[2]}" --compressed  --output captcha.jpg
+  curl -s 'https://u.njtech.edu.cn/cas/captcha.jpg?0.29064673903582183' -H "Cookie: JSESSIONID=${para[1]}; insert_cookie=${para[2]}" --output captcha.jpg
   shasumCaptcha=`sha1sum captcha.jpg | cut -d ' ' -f 1`
-  para[5]=`sed -n "/^$shasumCaptcha/p" RainbowNjtechHomeKeys | cut -d ' ' -f 2`
+  para[5]=`sed -n "/^$shasumCaptcha/p" /home/jtp/ConnectNjtechHome/RainbowNjtechHomeKeys | cut -d ' ' -f 2`
 done
 
 echo ${para[@]} $shasumCaptcha
 
-curl -s -XPOST "https://u.njtech.edu.cn/cas/login;jsessionid=${para[1]}?service=https%3A%2F%2Fu.njtech.edu.cn%2Foauth2%2Fauthorize%3Fclient_id%3DOe7wtp9CAMW0FVygUasZ%26response_type%3Dcode%26state%3Dnjtech%26s%3Df682b396da8eb53db80bb072f5745232" -H "Cookie: JSESSIONID=${para[1]}; insert_cookie=${para[2]}" -d "username=${username}&password=${password}&captcha=${para[5]}&channelshow=%E4%B8%AD%E5%9B%BD%E7%94%B5%E4%BF%A1&channel=%40telecom&lt=${para[3]}&execution=${para[4]}&_eventId=submit" \ --compressed  -L | grep -o 'window.location.href = "/oauth2/logout?retUrl=https://i.njtech.edu.cn";' > /dev/null
+curl -s -XPOST "https://u.njtech.edu.cn/cas/login;jsessionid=${para[1]}?service=https%3A%2F%2Fu.njtech.edu.cn%2Foauth2%2Fauthorize%3Fclient_id%3DOe7wtp9CAMW0FVygUasZ%26response_type%3Dcode%26state%3Dnjtech%26s%3Df682b396da8eb53db80bb072f5745232" -H "Cookie: JSESSIONID=${para[1]}; insert_cookie=${para[2]}" -d "username=${username}&password=${password}&captcha=${para[5]}&channelshow=${channelshow}&channel=${channel}&lt=${para[3]}&execution=${para[4]}&_eventId=submit" -L | grep -o 'window.location.href = "/oauth2/logout?retUrl=https://i.njtech.edu.cn";' > /dev/null
 
-if [ $? -eq 0 ] ; then echo "Login Success." ; else echo "Login Fail. Retrying." ; bash login.bash ; fi
-
+if [ $? -eq 0 ] ; then echo "Login Success." ; else echo "Login Fail. Retrying." ; bash /home/jtp/ConnectNjtechHome/login.bash ; fi
