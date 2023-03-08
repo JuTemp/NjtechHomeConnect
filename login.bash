d03324081f02a6549548335f1603b688317f8f4f
JuTemp <111566853+JuTemp@users.noreply.github.com>
2023-03-08 14:20:39 +0800

    Commit230308

--- login.bash
+++ login.bash
@@ -3,6 +3,8 @@
 username="202221140000"
 password="passwd123456"
 max_retry=200
+max_try=3
+if [ "$1" ]; then this_try="$1"; else this_try=0; fi 
 
 # 中国电信
 channelshow="%E4%B8%AD%E5%9B%BD%E7%94%B5%E4%BF%A1"
@@ -34,4 +36,11 @@ echo ${para[@]} $shasumCaptcha
 
 curl -s -XPOST "https://u.njtech.edu.cn/cas/login;jsessionid=${para[1]}?service=https%3A%2F%2Fu.njtech.edu.cn%2Foauth2%2Fauthorize%3Fclient_id%3DOe7wtp9CAMW0FVygUasZ%26response_type%3Dcode%26state%3Dnjtech%26s%3Df682b396da8eb53db80bb072f5745232" -H "Cookie: JSESSIONID=${para[1]}; insert_cookie=${para[2]}" -d "username=${username}&password=${password}&captcha=${para[5]}&channelshow=${channelshow}&channel=${channel}&lt=${para[3]}&execution=${para[4]}&_eventId=submit" -L | grep -o 'window.location.href = "/oauth2/logout?retUrl=https://i.njtech.edu.cn";' > /dev/null
 
-if [ $? -eq 0 ] ; then echo "Login Success." ; else echo "Login Fail. Retrying." ; bash login.bash ; fi
+if [ $? -eq 0 ]
+  then echo "Login Success."
+else
+  if [ ${this_try} -gt ${max_try} ]
+    then echo "Max Try. Exit."
+    else echo "Login Fail. Retrying."; bash login.bash $(( $1 + 1 ))
+  fi
+fi
